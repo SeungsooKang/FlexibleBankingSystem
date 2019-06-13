62110dddf68036fc91d29460fcceabedcd6be5be
SeungsooKang <seungsoo@sheridancollege.ca>
2019-06-13 17:29:05 -0400

    Add AccountManager Class

---
+++ Assignment02/AccountManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class AccountManager
+    {
+        public BankAccount ConsolidateAccounts(BankAccount account1, BankAccount account2)
+        {
+            if (!account1.owner.OwnerID.Equals(account2.owner.OwnerID))
+                throw new Exception("The owners of both accounts are different. Cannot be consolidated.\n");
+            if (account1.AccountNumber.Equals(account2.AccountNumber))
+                throw new Exception("The same accounts cannot be consolidated.\n");
+
+            BankAccount consolidated = null;
+
+            return consolidated;
+        }
+    }
+}
--- Assignment02/Owner.cs
+++ Assignment02/Owner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Assignment02
 {
     class Owner
     {
+        public string OwnerID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
