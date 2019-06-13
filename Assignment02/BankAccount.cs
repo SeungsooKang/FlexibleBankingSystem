8b49fbd5ea36b702e69449a0f89e638b4c1b3ef9
SeungsooKang <seungsoo@sheridancollege.ca>
2019-06-13 15:25:03 -0400

    Create BankAccount and Owner Class

---
+++ Assignment02/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    enum Status
+    {
+        Active,
+        Closed
+    }
+    class BankAccount
+    {
+        public string AccountNumber { get; set; }
+
+        private double _balance;
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public Owner owner { get; set; }
+        public Status status { get; set; }
+
+        public BankAccount(double initialBalance)
+        {
+            
+        }
+
+
+
+    }
+}
